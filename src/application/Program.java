b34db5b2d1957556dfe5b4f453ae08b0c0aaee21
Jean Port <pierreanatc522@gmail.com>
2023-06-14 15:33:34 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import models.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		
+		Department objDepartment = new Department(1, "Cassino");
+		
+		System.out.println(objDepartment);
+	}
+
+}
